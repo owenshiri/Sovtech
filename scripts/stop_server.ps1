0bf94f92d5b1b3c819345f5d57fb787ffa061f90
sbicza01\c822642 <owen.shiri@atandardbank.co.za>
2020-11-27 09:23:01 +0200

    fix the pipeline

--- scripts/stop_server.ps1
+++ scripts/stop_server.ps1
@@ -1,6 +1,6 @@
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  #This loads mvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  
-cd /var/www/html/lit-test/bin/release/netcoreapp3.1/linux-x64/publish
+cd /var/www/lit-test/bin/release/netcoreapp3.1/linux-x64/publish
 pm2 stop lit-test
 pm2 delete lit-test
